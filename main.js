12d88089e3509409c903849a52dbc3bf13a3d08c
Mary West <westmarye1219@gmail.com>
2018-09-22 12:52:00 -0500

    added text/buttons

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const secret = '1234567890', ;
+
+var index = 4;
+
+console.log('The character code ' + sentence.charCodeAt(index) + ' is equal to ' + sentence.charAt(index));
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+}
+
+let secretInput = document.getElementById("textarea")
+
+let wordsInput = document.getElementById("textarea2")
+
